3ef69347007e701a74ec842d686597d9b6401fe0
howard <howard60211@gmail.com>
2020-09-13 16:44:42 +0800

    29126 回家作業

---
+++ 755299/ask_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string name;
+	cout << "what's your name?";
+	cin >> name;
+	cout << "ooh! Hello " << name << "!\n";
+}
