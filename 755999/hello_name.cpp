46e60c236bb538626e2a4c6ca103a17be0baa50f
Jack <uihuihuih@gmail.com>
2020-09-17 19:40:33 +0800

    Add hello_name.cpp

---
+++ 755999/hello_name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char name[20];
+  cout << "Please your name:\n";
+  cin >> name;
+  cout << "Hello，";
+  cout << name;
+  cout << "\n";
+}
