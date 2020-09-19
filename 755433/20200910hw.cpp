8c0551338c8de7704fc0a1c57e22600359cba2e8
erichuang95 <a755433@stdmail.nssh.ntpc.edu.tw>
2020-09-19 22:20:23 +0800

    修改 755433黃國瑜 程式作業

--- 755433/20200910hw.cpp
+++ 755433/20200910hw.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main() {
-    string name;
+    //輸出
     cout << "請輸入你的名字:";
+    
+    //輸入名字
+    string name;
     cin >> name;
+
+    //總輸出
     cout << "Hello," << name << "!";
 }
