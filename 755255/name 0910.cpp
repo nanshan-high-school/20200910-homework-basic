80aa7cbcd3f225c2beef6ce6706a4927904df08d
陳可烜 <allan092500@gmail.com>
2020-09-20 16:45:18 +0800

    add a file

---
+++ 755255/name 0910.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    cout << "請輸入你的名字\n";
+    cin  >> name;
+    cout << "你好!";
+    cout << name;
+    cout << "，歡迎回到你的Repl.it。";
+}
