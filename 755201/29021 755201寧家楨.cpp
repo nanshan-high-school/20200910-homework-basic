0c55d6a9fb6b8d2180efd954e777c56cfa595cb2
Ericning <ningj6137@gmail.com>
2020-09-20 21:56:55 +0800

    755201 作業.

---
+++ 755201/29021 755201寧家楨.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char name[20];
+
+    cout << "½Ð¿é¤J¦W¦r:\n ";
+    cin >> name;
+
+    cout << "Hello¡A";
+    cout << name;
+    cout << "\n";
+}
