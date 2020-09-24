345300c2ee2b60ee02efbdc241458f79b58b0265
pigeo <anthony79@apps.ntpc.edu.tw>
2020-09-24 19:12:19 +0800

    feat: add 755293 code

---
+++ 755293/20200924-homework.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char name[30];
+    cout << "Nice to meet you. What's your name?\n";
+    cin >> name;
+    cout << "Hello, " << name << endl;
+}
